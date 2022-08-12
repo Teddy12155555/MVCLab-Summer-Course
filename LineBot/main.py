130fe1e64a4f82ee3d2026f41fb346c859394b4e
justbuyyal <wen19990116@gmail.com>
2022-08-13 00:17:41 +0800

    CodeFix: main.py,LineBot Structure Initialization; gitignore; AddFile: .env

--- LineBot/main.py
+++ LineBot/main.py
@@ -1,74 +1,38 @@
-from os import abort
-from fastapi import FastAPI, Request, Response, APIRouter, HTTPException, Header
-from typing import Optional, Text
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+import os
+from fastapi import FastAPI, Request, HTTPException
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
 app = FastAPI()
 
-router = APIRouter(
-    prefix="/webhooks",
-    tags=["chatbot"],
-    responses={404: {"description": "Not found"}},
-)
+CHANNEL_TOKEN = os.getenv('LINE_TOKEN')
+CHANNEL_SECRET = os.getenv('LINE_SECRET')
 
-CHANNEL_ACCESS_TOKEN = 'asFNhw2Th+xWFsOxcaJ7MhL+Et9OKoAQKnsjgtEbevVkS0u4E2fXRavGJl6cCYEWmydyPPfWO4v75zus0ft3H1mx/ngBFrit5qw2848xYGBT/OH8ZG/bEIFNLU4VBMqbt/Q7lS/hmIQmKzZydO09pwdB04t89/1O/w1cDnyilFU='
-CHANNEL_SECRET = 'c31eff1a497b39443fefeb349c826638'
-CHANNEL_ID = 'U9074d7f0b8a8f845592e778a48b73a17'
+My_LineBotAPI = LineBotApi(CHANNEL_TOKEN) # Connect Your API to Line Developer API by Token
+handler = WebhookHandler(CHANNEL_SECRET) # Event handler connect to Line Bot by Secret key
 
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN) # Add your line 
-handler = WebhookHandler(CHANNEL_SECRET) # Add your line developer channel secret key
+'''
+For first testing, you can comment the code below after you check your linebot can send you the message below
+'''
+CHANNEL_ID = os.getenv('LINE_UID') # For any message pushing to or pulling from Line Bot using this ID
+My_LineBotAPI.push_message(CHANNEL_ID, TextSendMessage(text='If you see this, you can start now')) # Push a testing message
 
-line_bot_api.push_message(CHANNEL_ID, TextSendMessage(text='You Can Start Now'))
-
-@router.post('/callback')
-def callback(request:Request, x_line_signature: str = Header(None)):
-    body = request.body()
+@app.post('/')
+async def callback(request: Request):
+    body = await request.body() # Get request
+    signature = request.headers.get('X-Line-Signature', '') # Get message signature from Line Server
     try:
-        handler.handle(body.decode("utf-8"), x_line_signature)
+        handler.handle(body.decode('utf-8'), signature) # Handler handle any message from LineBot and 
     except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="chatbot handle body error.")
+        raise HTTPException(404, detail='LineBot Handle Body Error !')
     return 'OK'
 
+# All message events are handling at here !
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-
-
-# @app.post('/message')
-# async def hello_word(request: Request):
-#     signature = request.headers['X-Line-Signature']
-#     body = await request.body()
-    
-#     try:
-#       handler.handle(body.decode('UTF-8'), signature)
-#     except InvalidSignatureError:
-#         print("Invalid signature. Please check your channel access token/channel secret.")
-#     return 'OK'
-
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#         if event.message.text == 'hello' : 
-#             sendMessage(event,"hello, here you are")
-#         else:
-#             echo(event)
-    
-# def echo(event):
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=event.message.text))
-        
-# def sendMessage(event,message):
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=message))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app= 'main:app', host= '0.0.0.0', port= 8787, reload= True) # Default host = 127.0.0.1, port = 8000
+def handle_textmessage(event):
+    message = event.message.text
+    My_LineBotAPI.reply_message(
+        event.reply_token,
+        message
+    )
