7fed57a8618df2e071230637a8bdb63bca42ccf1
justbuyyal <wen19990116@gmail.com>
2022-08-14 22:47:38 +0800

    CodeFix: LineBot/main.py, Fully complete for pokemon linebot; PyQuery, Add pyquery downloading img

--- LineBot/main.py
+++ LineBot/main.py
@@ -1,11 +1,34 @@
+from email import message
 import os
+import re
+import json
+import glob
 from dotenv import load_dotenv
+from pyquery import PyQuery
 from fastapi import FastAPI, Request, HTTPException
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
 load_dotenv() # Load your local environment variables
+
+# Pokedex Link
+pokemons_link = 'https://pokemondb.net/pokedex/all'
+# Get all info from link (html)
+doc = PyQuery(url=pokemons_link).find('td').find('span').children()
+# Create My_pokedex Images Dict
+pokemons_imgs = dict()
+# For filter the empty image from link
+empty_img_url = 'https://img.pokemondb.net/s.png'
+# Add each pokemon img url into dict
+for poke in doc:
+    poke = PyQuery(poke)
+    # Filter empty img
+    if not re.match(poke.attr('data-src'), empty_img_url):
+        poke_url = poke.attr('data-src') # Attribute['data-src'] value
+        poke_name = str(poke_url).split('/')[-1][:-4].lower() # Get lower case of a pokemon name & filter .png behind
+        # Save a pokemon's img info
+        pokemons_imgs[poke_name] = poke_url
 
 app = FastAPI()
 
@@ -19,7 +42,7 @@ handler = WebhookHandler(CHANNEL_SECRET) # Event handler connect to Line Bot by 
 For first testing, you can comment the code below after you check your linebot can send you the message below
 '''
 CHANNEL_ID = os.getenv('LINE_UID') # For any message pushing to or pulling from Line Bot using this ID
-My_LineBotAPI.push_message(CHANNEL_ID, TextSendMessage(text='If you see this, you can start now')) # Push a testing message
+# My_LineBotAPI.push_message(CHANNEL_ID, TextSendMessage(text='Welcome to my pokedex !')) # Push a testing message
 
 @app.post('/')
 async def callback(request: Request):
@@ -31,15 +54,130 @@ async def callback(request: Request):
         raise HTTPException(404, detail='LineBot Handle Body Error !')
     return 'OK'
 
+# Events for message reply
+my_event = ['#getpokemon', '#mypokemon', '#addpokemon', '#delpokemon', '#help']
+# My pokemon datas
+my_pokemons = dict()
+poke_file = 'my_pokemons.json'
+# Load local json file if exist
+if os.path.exists(poke_file):
+    with open(poke_file, 'r') as f:
+        my_pokemons = json.load(f)
+
 # All message events are handling at here !
 @handler.add(MessageEvent, message=TextMessage)
 def handle_textmessage(event):
+    ''' Basic Message Reply
     message = TextSendMessage(text= event.message.text)
     My_LineBotAPI.reply_message(
         event.reply_token,
         message
     )
-
+    '''
+    recieve_message = str(event.message.text).split(' ')
+    case_ = recieve_message[0].lower().strip()
+    # Case 1: get pokemon
+    if re.match(my_event[0], case_):
+        pokename = recieve_message[1].lower().strip()
+        if pokename in pokemons_imgs.keys():
+            url = pokemons_imgs[pokename]
+            # Return image
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                ImageSendMessage(
+                    original_content_url=url,
+                    preview_image_url=url
+                )
+            )
+        else:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(text='Pokemon not found in pokedex !')
+            )
+    # Case 2: show my pokemons (if existed)
+    elif re.match(my_event[1], case_):
+        if len(my_pokemons):
+            message_arr = [] # Message size must between 1 to 5 !
+            message_arr.append(TextSendMessage(text='Here is your pokemons :'))
+            for pokename, url in my_pokemons.items():
+                if len(message_arr) == 5:
+                    # Notion: reply_token can only be used once !
+                    My_LineBotAPI.reply_message(
+                        event.reply_token,
+                        message_arr
+                    )
+                    message_arr.clear()
+                # Send Poke name
+                message_arr.append(TextSendMessage(text= pokename))
+            # Reply multiple messages (if existed)
+            for message in message_arr:
+                My_LineBotAPI.push_message(
+                    CHANNEL_ID,
+                    message
+                )
+        else:
+            My_LineBotAPI.push_message(
+                event.reply_token,
+                TextSendMessage(text='You don\'t have any pokemon')
+            )
+    # Case 3: add a pokemon into my pokedex
+    elif re.match(my_event[2], case_):
+        pokename = recieve_message[1].lower().strip()
+        if pokename in pokemons_imgs.keys():
+            if pokename in my_pokemons.keys():
+                My_LineBotAPI.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=f'You have already add {pokename} into your pokedex !')
+                )
+            else:
+                # Add a new pokemon into my pokedex
+                my_pokemons[pokename] = pokemons_imgs[pokename]
+                with open(poke_file, 'w') as f:
+                    json.dump(my_pokemons, f, indent=4)
+                # Reply successful message
+                My_LineBotAPI.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=f'Successful add {pokename} into your pokedex !')
+                )
+        else:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'Pokemon "{pokename}" not found in global pokedex !')
+            )
+    # Case 3: Delete a pokemon from my pokedex
+    elif re.match(my_event[3], case_):
+        pokename = recieve_message[1].lower().strip()
+        if pokename in my_pokemons.keys():
+            # Remove an existed pokemon in my pokedex
+            my_pokemons.pop(pokename)
+            with open(poke_file, 'w') as f:
+                json.dump(my_pokemons, f, indent=4)
+            # Reply successful message
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'Successful delete {pokename} from your pokedex !')
+            )
+        else:
+            My_LineBotAPI.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'Pokemon "{pokename}" not found in your pokedex !')
+            )
+    # Help command for listing all commands to user
+    elif re.match(my_event[4], case_):
+        command_describtion = 'Commands:\n\
+        #getpokemon <pokemon\'s name>\n\t-->Show this pokemon\'s name & Image for you if existed !\n\
+        #mypokemon\n\t-->List all pokemons in your pokedex if existed !\n\
+        #addpokemon <pokemon\'s name>\n\t-->Add a pokemon from global pokedex into your pokedex if existed !\n\
+        #delpokemon <pokemon\'s name>\n\t-->Delete a pokemon from your pokedex if existed !\n'
+        My_LineBotAPI.reply_message(
+            event.reply_token,
+            TextSendMessage(text=command_describtion)
+        )
+    else:
+        My_LineBotAPI.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Welcome to my pokedex ! Enter "#help" for commands !')
+        )
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app='main:app', reload=True, host='0.0.0.0', port=8787)
--- PyQuery/python_query_test.py
+++ PyQuery/python_query_test.py
@@ -5,9 +5,13 @@ https://itsmycode.com/python-urllib-error-httperror-http-error-403-forbidden/
 https://zwindr.blogspot.com/2017/12/python-pyquery.html
 https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/360045/ 
 '''
+import shutil
 from pyquery import PyQuery
 import ssl
 from urllib.request import Request, urlopen
+import re
+import requests
+from os import path
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -45,4 +49,44 @@ pokemons_name = pokemons_.find('.ent-name').text().split(' ')
 # print(pokemons_index, pokemons_name)
 
 pokedex = dict(zip(pokemons_index, pokemons_name))
-print(pokedex)
+# print(pokedex)
+
+# Pokedex Link
+pokemons_link = 'https://pokemondb.net/pokedex/all'
+# Get all info from link (html)
+doc = PyQuery(url=pokemons_link)
+# Get pokemons img src url
+pokemons_urls = doc.find('td').find('span').children()
+pokemons_img = dict()
+# print(pokemons_urls)
+empty_img = 'https://img.pokemondb.net/s.png' # Filter the empty img
+for item in pokemons_urls:
+    item = PyQuery(item)
+    if not re.match(item.attr('data-src'), empty_img):
+        url = item.attr('data-src')
+        poke_name = str(url).split('/')[-1][:-4]
+        pokemons_img[poke_name] = url
+# print(pokemons_img)
+
+# Save the img to db
+folder = './PyQuery/pokemons/'
+for img_url in pokemons_img.values():
+    img_name = folder + str(img_url).split('/')[-1]
+    if path.exists(img_name):
+        print('Continuing ...')
+        continue
+    img_data = requests.get(url=img_url, stream=True)
+    if img_data.status_code == 200:
+        # Set decode_content be True, otherwise the downloaded image file's size will be zero
+        img_data.raw.decode_content = True
+        with open(img_name, 'wb') as f:
+            shutil.copyfileobj(img_data.raw, f)
+        print('Image sucessfully Downloaded: ',img_name)
+    else:
+        print('Image Couldn\'t be retreived')
+
+# Regular Expression match testing
+str1 = 'hello'
+str2 = '   HellO  '
+print(re.match(str1.lower(), str2.lower())) # Return None
+print(re.match(str1.lower().strip(), str2.lower().strip())) # Return <0, 5>
